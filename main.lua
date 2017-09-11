443c55bbba906203fbab1f455af7f0b56e59f5b1
scottrules44 <scottrules44@gmail.com>
2017-09-11 16:05:42 -0500

    Bug fixes
    Bug Fixes

--- main.lua
+++ main.lua
@@ -1,10 +1,8 @@
 local likeButton = require("plugin.likeButton")
-
+local facebook = require("plugin.facebook.v4a")
 local bg = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
 bg:setFillColor( 0,0,1 )
 
 local title = display.newText( "Like Button Plugin", display.contentCenterX, 30, native.systemFontBold, 20 )
 
 local myLikeButton = likeButton.newButton({x = display.contentCenterX, y= display.contentCenterY, id = "https://www.facebook.com/CoronaLabs"})
-
-print(myLikeButton:getSize())
